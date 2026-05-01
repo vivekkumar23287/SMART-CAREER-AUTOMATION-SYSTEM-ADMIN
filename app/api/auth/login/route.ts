b20e058b5315ab83afb8d27047fe415323a29b75
VIVEK KUMAR <blackwidi414@gmail.com>
2026-05-01 12:29:28 +0530

    first commit

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -17,7 +17,6 @@ export async function POST(req: Request) {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax',
-        maxAge: 60 * 60 * 24 * 7, // 1 week
         path: '/',
       });
 
